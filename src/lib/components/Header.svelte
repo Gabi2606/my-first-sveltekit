da89f0a5dd964be2216bf9e2417b22d389d895c4
Gabriela_Pineiro <gabriela.pineiro@factoriaf5.org>
2023-07-03 11:18:35 +0200

    first commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,4 @@
+<nav>
+    <li><a href="/static">Home</a></li>
+    <li><a href="/about">About</a></li>
+</nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import image1 from "$lib/images/image1.webp"
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<img src={image1} alt="playa">
